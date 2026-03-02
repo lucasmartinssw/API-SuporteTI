fa3d02eaf5ace1fcead07d7295d699e8fa1520b7
Lucas Martins <lucasrmartins40@gmail.com>
2026-03-02 19:00:44 -0300

    fix: Update path users end-point

--- app/database.py
+++ app/database.py
@@ -13,7 +13,7 @@ def get_db_connection():
             host="127.0.0.1",
             user="root",
             password="root",
-            database="projetofinal"
+            database="projetofinal2"
         )
         # Entrega a conexão para a requisição atual
         yield conn
--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from .routers import usuarios, auth, chamados
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API Final Project", description="API for Final Project", version="1.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Em produção, coloque a URL do seu front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -11,4 +19,4 @@ def root():
 
 app.include_router(auth.router)
 app.include_router(usuarios.router)
-app.include_router(chamados.router)
+app.include_router(chamados.router) 
